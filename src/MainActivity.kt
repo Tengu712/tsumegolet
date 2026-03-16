54ea366f21bb11364df3cd684899b98aa1aa7339
Tengu712 <genreihoutengu@gmail.com>
2026-03-16 12:11:14 +0900

    add add

--- src/MainActivity.kt
+++ src/MainActivity.kt
@@ -25,11 +25,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.skdassoc.tsumegolet.model.BoardCoord
-import com.skdassoc.tsumegolet.model.BoardSize
 import com.skdassoc.tsumegolet.model.KifuData
-import com.skdassoc.tsumegolet.model.Stone
-import com.skdassoc.tsumegolet.model.StoneColor
 import com.skdassoc.tsumegolet.scene.EditScene
 import com.skdassoc.tsumegolet.scene.ListScene
 import com.skdassoc.tsumegolet.scene.QuestionScene
@@ -46,6 +42,8 @@ private val appColorScheme =
     lightColorScheme(
         primary = Color.Black,
         onPrimary = Color.White,
+        primaryContainer = Color.Black,
+        onPrimaryContainer = Color.White,
         background = Color.White,
         onBackground = Color.Black,
         surface = Color.White,
@@ -55,73 +53,7 @@ private val appColorScheme =
 @Composable
 private fun Content() {
     // TODO: 記録されたデータを参照する
-    var kifuList by remember {
-        mutableStateOf(
-            listOf(
-                KifuData(
-                    "詰め碁 1",
-                    boardSize = BoardSize.Nineteen,
-                    offsetCol = 0,
-                    offsetRow = 0,
-                    extentCols = 7,
-                    extentRows = 7,
-                    stones =
-                        listOf(
-                            Stone(BoardCoord(0, 6), StoneColor.Black),
-                            Stone(BoardCoord(1, 6), StoneColor.Black),
-                            Stone(BoardCoord(2, 6), StoneColor.Black),
-                            Stone(BoardCoord(0, 5), StoneColor.White),
-                            Stone(BoardCoord(1, 5), StoneColor.White),
-                            Stone(BoardCoord(2, 5), StoneColor.Black),
-                            Stone(BoardCoord(0, 4), StoneColor.White),
-                            Stone(BoardCoord(1, 4), StoneColor.Black),
-                        ),
-                    answer = BoardCoord(0, 3),
-                    answerTurn = StoneColor.Black,
-                ),
-                KifuData(
-                    "詰め碁 2",
-                    boardSize = BoardSize.Nineteen,
-                    offsetCol = 12,
-                    offsetRow = 12,
-                    extentCols = 7,
-                    extentRows = 7,
-                    stones =
-                        listOf(
-                            Stone(BoardCoord(18, 18), StoneColor.Black),
-                            Stone(BoardCoord(17, 18), StoneColor.Black),
-                            Stone(BoardCoord(18, 17), StoneColor.White),
-                            Stone(BoardCoord(17, 17), StoneColor.Black),
-                            Stone(BoardCoord(16, 17), StoneColor.White),
-                            Stone(BoardCoord(18, 16), StoneColor.White),
-                        ),
-                    answer = BoardCoord(17, 16),
-                    answerTurn = StoneColor.White,
-                ),
-                KifuData(
-                    "詰め碁 3",
-                    boardSize = BoardSize.Nineteen,
-                    offsetCol = 5,
-                    offsetRow = 3,
-                    extentCols = 9,
-                    extentRows = 6,
-                    stones =
-                        listOf(
-                            Stone(BoardCoord(7, 8), StoneColor.Black),
-                            Stone(BoardCoord(8, 8), StoneColor.Black),
-                            Stone(BoardCoord(9, 8), StoneColor.White),
-                            Stone(BoardCoord(7, 7), StoneColor.White),
-                            Stone(BoardCoord(8, 7), StoneColor.Black),
-                            Stone(BoardCoord(9, 7), StoneColor.White),
-                            Stone(BoardCoord(7, 6), StoneColor.Black),
-                            Stone(BoardCoord(8, 6), StoneColor.White),
-                        ),
-                    answer = BoardCoord(6, 8),
-                    answerTurn = StoneColor.White,
-                ),
-            )
-        )
-    }
+    var kifuList by remember { mutableStateOf(emptyList<KifuData>()) }
     val navController = rememberNavController()
 
     Surface(
@@ -135,7 +67,15 @@ private fun Content() {
                 }
             }
             composable("list") {
-                ListScene(kifuList) { index -> navController.navigate("question/$index") }
+                ListScene(
+                    kifuList,
+                    onSelect = { index -> navController.navigate("question/$index") },
+                    onAdd = {
+                        val newIndex = kifuList.size
+                        kifuList = kifuList.toMutableList().also { it.add(KifuData(title = "")) }
+                        navController.navigate("edit/$newIndex")
+                    },
+                )
             }
             composable("question/{index}") { backStackEntry ->
                 val index = backStackEntry.arguments?.getString("index")?.toIntOrNull()
